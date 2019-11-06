35c7135ac209c6904530494b6eec08c31b4dec97
deiberepos <52751415+deiberepos@users.noreply.github.com>
2019-11-06 14:57:23 -0500

    Creación de la primera tabla y la conección a base de datos

---
+++ MyLeasing.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLeasing.Web.Data.Entities;
+
+namespace MyLeasing.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
